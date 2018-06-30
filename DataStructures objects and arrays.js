5302593f93bac179a6609d4683b6917634aebdfa
venkat kondrasu <venkat.kondrasu@gmail.com>
2018-06-30 18:01:19 +0530

    2. Reversing an array

--- DataStructures objects and arrays.js
+++ DataStructures objects and arrays.js
@@ -1,4 +1,6 @@
-//1. The sum of a range
+/*
+  1. The sum of a range
+*/
 
 function range(start, end, step){
   var res = [];
@@ -32,3 +34,32 @@ console.log(range(5, 2, -1));
 // → [5, 4, 3, 2]
 console.log(sum(range(1, 10)));
 // → 55
+
+
+/*
+  2. Reversing an array
+*/
+
+function reverseArray(ar){
+  var result = [];
+  for(var i = ar.length -1; i >=0; i--){
+    result.push(ar[i]);
+  }
+  return result;
+}
+
+function reverseArrayInPlace(ar){
+  var temp;
+  for(var i = 0, j = ar.length -1; i<j; i++,j--){
+    temp = ar[i];
+    ar[i]  = ar[j];
+    ar[j] = temp;
+  }
+}
+
+console.log(reverseArray(["A", "B", "C"]));
+// → ["C", "B", "A"];
+let arrayValue = [1, 2, 3, 4, 5];
+reverseArrayInPlace(arrayValue);
+console.log(arrayValue);
+// → [5, 4, 3, 2, 1]
